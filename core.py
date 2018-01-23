134380e3e1e3c740df51934a45f898898287481e
raizella <raizella@gmail.com>
2018-01-23 11:33:32 -0500

    Improve style
    Changed code style to match pylint and added some comments.

--- core.py
+++ core.py
@@ -9,7 +9,7 @@ This implementation of a search engine is composed of three classes:
 import functools
 import json
 import re
-from typing import List, Set, Dict, Generator
+from typing import Dict, Generator, List, Set
 
 from nltk.stem import PorterStemmer
 
@@ -24,14 +24,14 @@ class Analyzer:
             self.stopwords = []
         else:
             self.stopwords = stopwords
-        self.ps = PorterStemmer()
+        self.port_stemmer = PorterStemmer()
 
     def get_terms(self, stream) -> Generator[str, None, None]:
         """Takes in a stream of words and returns a list of terms."""
         words = filter(None, re.split(r'[^a-z0-9]', stream.lower()))
         for word in words:
             if word not in self.stopwords:
-                stemmed_word = self.ps.stem(word)
+                stemmed_word = self.port_stemmer.stem(word)
                 if stemmed_word:
                     yield stemmed_word
 
@@ -50,10 +50,10 @@ class Index:
         """Parses the collection file to construct the internal
         index structure (stored as the attribute self._index)
         """
-        with open(collection_fp) as f:
+        with open(collection_fp) as file_text:
             # TODO: What if the file won't fit in memory? (We can ignore this for now)
-            content: str = f.read()
-        for page in self._parse_xml(content):
+            content: str = file_text.read()
+        for page in self.parse_xml(content):
             stream = page['stream']
             terms = self.analyzer.get_terms(stream)
 
@@ -69,7 +69,9 @@ class Index:
                     self.inverted_index[term] = {}
                     self.inverted_index[term][page['id']] = [pos]
 
-    def _parse_xml(self, s) -> Generator[Dict, None, None]:
+    def parse_xml(self, s) -> Generator[Dict, None, None]:
+        """Parses string in XML format to Dict of pages with fields 'id', 'title' and 'stream'
+         (stream is the text of the document)"""
         # TODO: If we use a different data dump, we should use a library like lxml
         while s.find('<page>') > -1:
             start_i = s.find('<id>')
@@ -92,17 +94,17 @@ class Index:
         """Constructs the index from the input index file at
         the given path."""
         # self._index = ...
-        with open(index_filepath, 'r') as f:
-            self.inverted_index = json.load(f)
-        with open(title_filepath, 'r') as f:
-            self.title_index = json.load(f)
+        with open(index_filepath, 'r') as file_text:
+            self.inverted_index = json.load(file_text)
+        with open(title_filepath, 'r') as file_text:
+            self.title_index = json.load(file_text)
 
     def write(self, index_filepath: str, title_filepath: str) -> None:
         """Writes the index to disk at the given folder"""
-        with open(index_filepath, 'w') as f:
-            json.dump(self.inverted_index, f)
-        with open(title_filepath, 'w') as f:
-            json.dump(self.title_index, f)
+        with open(index_filepath, 'w') as file_text:
+            json.dump(self.inverted_index, file_text)
+        with open(title_filepath, 'w') as file_text:
+            json.dump(self.title_index, file_text)
 
     def search(self, query_string: str) -> List[int]:
         """Searches the index for the file"""
@@ -124,6 +126,7 @@ class Index:
 
 
 class Query:
+    """ Saves query string and implements match """
     def __init__(self, query_string: str):
         self.query_string = query_string
 
@@ -134,6 +137,7 @@ class Query:
 
 
 class OneWordQuery(Query):
+    """ one word query """
     def match(self, index: Index) -> Set[int]:
         terms = index.analyzer.get_terms(self.query_string)
         # A single word may be split into multiple terms!
@@ -147,6 +151,7 @@ class FreeTextQuery(OneWordQuery):
 
 
 class PhraseQuery(Query):
+    """ Query with one or more words """
     def match(self, index: Index) -> Set[int]:
         # TODO (Issue #3)
         query_terms = index.analyzer.get_terms(self.query_string)
@@ -157,24 +162,26 @@ class PhraseQuery(Query):
             matches = set(curr_indices.keys()).intersection(set(next_indices.keys()))
             for doc_id in matches:
                 doc_id_list = self.merge_lists(curr_indices[doc_id], next_indices[doc_id])
-                if len(doc_id_list) > 0:
+                if doc_id_list:
                     temp_next[doc_id] = doc_id_list
             curr_indices = temp_next
         return set(curr_indices.keys())
 
-    def merge_lists(self, curr_indices : List, next_indices : List) -> List:
+    def merge_lists(self, curr_indices: List, next_indices: List) -> List:
+        ''' helper function for match'''
         to_return = []
-        for i in range(len(curr_indices)):
-            for j in range(len(next_indices)):
-                if curr_indices[i] + 1 == next_indices[j]:
-                    to_return.append(next_indices[j])
+        for curr_index in curr_indices:
+            for next_index in next_indices:
+                if curr_index + 1 == next_index:
+                    to_return.append(next_index)
+
         return to_return
 
 
 class BooleanQuery(Query):
+
     def match(self, index: Index) -> Set[int]:
         # TODO (Issue #2)
-        #terms = index.analyzer.get_terms(self.query_string)
         # A single word may be split into multiple terms!
         parsed_query = bool_expr_ast(self.query_string)
 
@@ -189,11 +196,12 @@ class BooleanQuery(Query):
         matches = [self.helper(operand, index) for operand in operands]
         if operator == 'OR':
             return functools.reduce(lambda s, t: s.union(t), matches)
-        else:  # AND
-            return functools.reduce(lambda s, t: s.intersection(t), matches)
+
+        return functools.reduce(lambda s, t: s.intersection(t), matches)
 
 
 class WildcardQuery(Query):
+    """ Query which can use * as a wildcard to match any token"""
     def match(self, index: Index) -> Set[int]:
         # TODO (Issue #1)
         raise NotImplementedError
@@ -216,13 +224,14 @@ class QueryFactory:
 
 
 class Scorer:
+    """ Uses TF-IDF ranking to rank matches"""
     # TODO (Issue 5): TF-IDF ranking
     pass
 
 
 def main():
-    with open('data/part1/stopWords.dat') as f:
-        test_stopwords = [line.rstrip('\n') for line in f]
+    with open('data/part1/stopWords.dat') as file_text:
+        test_stopwords = [line.rstrip('\n') for line in file_text]
     test_index = Index(test_stopwords)
     print('Loading Index')
     # Uncomment this call to read and comment the call to parse to read
